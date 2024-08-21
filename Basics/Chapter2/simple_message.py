5e3dfbd41265a812fb30f396ba8a9206524f93c7
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-21 22:47:44 +0200

    Create simple_message.py

---
+++ Basics/Chapter2/simple_message.py
@@ -0,0 +1,16 @@
+message = "simple message"
+print(message)
+
+message = 'not-so simple message'
+print(message)
+
+btw = [1,2,3]
+btw2 = btw2
+
+print(btw)
+print(btw2)
+
+btw = [0,1,2,3]
+
+print(btw)
+print(btw2)
