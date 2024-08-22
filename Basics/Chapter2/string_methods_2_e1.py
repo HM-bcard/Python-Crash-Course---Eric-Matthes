f0f053b721891fc0a05be6fbdcbddd76adda14cd
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-22 04:24:03 +0200

    Update string_methods_2_e1.py

--- Basics/Chapter2/string_methods_2_e1.py
+++ Basics/Chapter2/string_methods_2_e1.py
@@ -1,1 +1,4 @@
-
+string_with_whitespaces = 'Look here        deer         is          on        the       road'
+print(string_with_whitespaces)
+string_without_whitespaces = string_with_whitespaces.rstrip()
+print(string_without_whitespaces)
