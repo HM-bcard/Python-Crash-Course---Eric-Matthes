5862550eb99c6ecb0cd5df0135c2905c68cb6c00
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-22 04:21:06 +0200

    Update string_methods_3_e1.py

--- Basics/Chapter2/string_methods_3_e1.py
+++ Basics/Chapter2/string_methods_3_e1.py
@@ -1,1 +1,8 @@
 print("Languages:\n\tPython\n\tC\n\tJavaScript")
+ino = 'ino'
+shishi = 'shishi'
+inoshishi = f"{ino} {shishi}"
+print(inoshishi.title())
+print(f"{ino.title()} {shishi}")
+# adding a tab with \t
+print(f"{ino.title()} \t {shishi}")
