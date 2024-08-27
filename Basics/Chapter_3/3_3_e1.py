a5f93ee60d2d82842b510893d17af5c664863679
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-27 08:56:03 +0200

    Create 3_3_e1.py

---
+++ Basics/Chapter_3/3_3_e1.py
@@ -0,0 +1,4 @@
+mode_of_transportation = ['Motorcycle','Car','Chopper']
+print(f"I will soon own a Dodge Challenger {mode_of_transportation[1].lower()}.")
+print(f"I will soon own a BMW  {mode_of_transportation[0].lower()}.")
+print(f"I will soon own an Indian {mode_of_transportation[2].lower()}.")
