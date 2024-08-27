bdf25ed646a2ca86aa7e9450580af14ac1ffd6f5
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-27 09:20:22 +0200

    Create 3_5_e1.py

---
+++ Basics/Chapter_3/3_5_e1.py
@@ -0,0 +1,11 @@
+guest_list = ['Mark Manson','Marilyn Manson','A Mansion']
+print(f"Thee honourable guest {guest_list[0].title()} is hereby invited to dinner at 5 o'clock")
+print(f"Thee honourable guest {guest_list[1].title()} is hereby invited to dinner at 5 o'clock")
+print(f"Thee honourable guest {guest_list[2].title()} is hereby invited to dinner at 5 o'clock")
+
+print(f"Unfortuantely the guest {guest_list[2].title()} will not be able to grace us with it's presence at it is stationary.")
+guest_list[2] = 'A boat'
+
+print(f"Thee honourable guest {guest_list[0].title()} is hereby invited to dinner at 5 o'clock")
+print(f"Thee honourable guest {guest_list[1].title()} is hereby invited to dinner at 5 o'clock")
+print(f"Thee honourable guest {guest_list[2].title()} is hereby invited to dinner at 5 o'clock")
