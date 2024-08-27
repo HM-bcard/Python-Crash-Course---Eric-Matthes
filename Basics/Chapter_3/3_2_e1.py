7f973a34119326df9ef98f7b1a49f5f640ead7cd
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-27 08:52:39 +0200

    Create 3_2_e1.py

---
+++ Basics/Chapter_3/3_2_e1.py
@@ -0,0 +1,5 @@
+names = ['Mark','John','Hugh','Mike']
+print(f"Oh, Hi {names[0].title()}")
+print(f"Oh, Hi {names[1].title()}")
+print(f"Oh, Hi {names[2].title()}")
+print(f"Oh, Hi {names[3].title()}")
