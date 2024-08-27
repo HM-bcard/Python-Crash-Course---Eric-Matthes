f15b672c182b8343127f8fb59278d9591e28fd78
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-27 09:15:47 +0200

    Update List_examples2_e1.py

--- Basics/Chapter_3/List_examples2_e1.py
+++ Basics/Chapter_3/List_examples2_e1.py
@@ -1,3 +1,11 @@
 best_bikes = ['BWM','Honda','Suzuki','Aprilia']
 best_bikes[1] = 'MotoGuzzi'
 print(best_bikes[1])
+
+
+best_bikes.append('Victory')
+print(best_bikes)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
