72369eae29a2a09587e3f62100eb866e03ce185b
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-27 09:18:11 +0200

    Create 3_4_e1.py

---
+++ Basics/Chapter_3/3_4_e1.py
@@ -0,0 +1,4 @@
+guest_list = ['Mark Manson','Marilyn Manson','A Mansion']
+print(f"Thee honourable guest {guest_list[0].title()} is hereby invited to dinner at 5 o'clock")
+print(f"Thee honourable guest {guest_list[1].title()} is hereby invited to dinner at 5 o'clock")
+print(f"Thee honourable guest {guest_list[2].title()} is hereby invited to dinner at 5 o'clock")
