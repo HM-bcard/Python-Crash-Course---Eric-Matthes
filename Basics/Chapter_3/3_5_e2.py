1f266e66623b84cbff746dcf30e640c5c37ee973
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-31 09:21:17 +0200

    Create 3_5_e2.py

---
+++ Basics/Chapter_3/3_5_e2.py
@@ -0,0 +1,13 @@
+guests = ['Michael Douglas',"Kirk Douglas", "Charlie Sheen", "Michael Sheen"]
+print(f"Sir {guests[0]}, please consider yourself invited to the party.")
+print(f"Sir {guests[2]}, please consider yourself invited to the party.")
+print(f"Sir {guests[1]}, please consider yourself invited to the party.")
+print(f"Sir {guests[3]}, please consider yourself invited to the party.")
+
+print(f"We are sorry to inform that sir {guests[1]} will not be able to attend")
+
+guests[1] = "Emory Tot"
+print(f"Sir {guests[0]}, please consider yourself invited to the party.")
+print(f"Sir {guests[2]}, please consider yourself invited to the party.")
+print(f"Sir {guests[1]}, please consider yourself invited to the party.")
+print(f"Sir {guests[3]}, please consider yourself invited to the party.")
