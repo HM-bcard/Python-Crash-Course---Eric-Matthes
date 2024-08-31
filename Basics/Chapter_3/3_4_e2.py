d34dc1fe9b17889b620797b77738f11abab63d31
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-31 09:13:54 +0200

    Update 3_4_e2.py

--- Basics/Chapter_3/3_4_e2.py
+++ Basics/Chapter_3/3_4_e2.py
@@ -1,2 +1,2 @@
 guests = ['Michael Douglas',"Kirk Douglas", "Charlie Sheen", "Michael Sheen"]
-print(f"Sir {guests[0], please consider yourself invited to the party.}")
+print(f"Sir {guests[0]}, please consider yourself invited to the party.")
