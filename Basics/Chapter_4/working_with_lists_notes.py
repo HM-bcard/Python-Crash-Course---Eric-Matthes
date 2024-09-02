faa1021f115dfc8fa47692c095186b21dd33f1ed
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-09-02 07:41:56 +0200

    Create working_with_lists_notes.py

---
+++ Basics/Chapter_4/working_with_lists_notes.py
@@ -0,0 +1,7 @@
+
+magicians = ['alice','david','derren']
+for magician in magicians:
+    print(f"Thank you {magician.title()} for the great performance.") # so it carries on the temporary variable
+    print(f"Please give a round of applause to {magician.upper()}\n")
+print("Thanks You Everyone for coming".title())
+
