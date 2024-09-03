28cb50846b2114c8b459b51efe445f811f4aff8c
HM-bcard <study.hub@pm.me>
2024-09-03 15:04:22 +0200

    Additional notes to chapter 4

--- Basics/Chapter_4/working_with_lists_notes.py
+++ Basics/Chapter_4/working_with_lists_notes.py
@@ -5,3 +5,7 @@ for magician in magicians:
     print(f"Please give a round of applause to {magician.upper()}\n")
 print("Thanks You Everyone for coming".title())
 
+digits = list(range(0,11))
+print(min(digits))
+print(max(digits))
+print(sum(digits))
