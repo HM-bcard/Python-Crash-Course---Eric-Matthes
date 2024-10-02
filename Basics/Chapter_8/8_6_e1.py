a8d294b05a05d5ef26f7545dfb23d133cbcbd8f8
HM-bcard <study.hub@pm.me>
2024-10-02 03:38:41 +0200

    exercises from chapter 8

---
+++ Basics/Chapter_8/8_6_e1.py
@@ -0,0 +1,23 @@
+def city_country():
+    city = input("Input the name of the city:")
+    country = input("Input the name of the country:")
+    return f"\"{city.title()}, {country.title()}\""
+
+pair1 = city_country()
+print(pair1)
+
+def city_country():
+    city = input("Input the name of the city:")
+    country = input("Input the name of the country:")
+    return f"\"{city.title()}, {country.title()}\""
+pair2 = city_country()
+print(pair2)
+
+def city_country():
+    city = input("Input the name of the city:")
+    country = input("Input the name of the country:")
+    return f"\"{city.title()}, {country.title()}\""
+
+pair3 = city_country()
+print(pair3)
+
