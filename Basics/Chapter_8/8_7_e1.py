0b7b72226326fc0959a78547fc771281ba06eaa0
HM-bcard <study.hub@pm.me>
2024-10-04 02:04:10 +0200

    exercises chapter 8

---
+++ Basics/Chapter_8/8_7_e1.py
@@ -0,0 +1,28 @@
+
+
+def make_album(artist=None,album=None,no_of_songs = None):
+    album_data = {}
+    artist = input("Input the name of the artist   ")
+    album = input("Input the name of the album   ")
+    album_data[artist] = album
+    return album_data
+album_1 = make_album()
+print(album_1)
+
+def make_album(artist=None,album=None,no_of_songs = None):
+    album_data = {}
+    artist = input("Input the name of the artist   ")
+    album = input("Input the name of the album   ")
+    album_data[artist] = album
+    return album_data
+album_2 = make_album()
+print(album_2)
+
+def make_album(artist=None,album=None,no_of_songs = None):
+    album_data = {}
+    artist = input("Input the name of the artist   ")
+    album = input("Input the name of the album   ")
+    album_data[artist] = album
+    return album_data
+album_3 = make_album()
+print(album_3)
