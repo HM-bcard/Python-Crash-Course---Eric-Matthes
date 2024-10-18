e110cdc14dc40220772bc20e096705ae1f983445
HM-bcard <study.hub@pm.me>
2024-10-18 23:29:31 +0200

    some notes chaPTER 9

--- Basics/Chapter_9/classes_notes_3_e1.py
+++ Basics/Chapter_9/classes_notes_3_e1.py
@@ -30,13 +30,53 @@ class Car:
 
 # inheriting class :
 
+class Battery:
+    """Subclass to represent the battery in the ElectriCar class"""
+    def __init__(self,battery_size=40):
+        """Initialize the battery's attributes"""
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """Print battery size description"""
+        print(f"This car has a {self.battery_size} kWh battery.")
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+        print(f"This car can go about {range} miles on a full charge.")
 
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
-        """Initialize attributes of the parent class."""
-        super().__init__(make, model, year)
+        """
+        Initialize attributes of the parent class.
+        Then initialize the attributes specific to an electric car
+        """
+        super().__init__(make, model, year) # super() function calls the 
+        # .__init__ method from the parent class
+        self.battery_size = 40
+        self.battery = Battery()
+        
+        '''
+    def describe_battery(self):
+        """Print battery size description"""
+        print(
+        f"The {self.make.title()} {self.model.title()} has a "
+        f"{self.battery_size}"  
+        f"Ah battery" 
+    )'''
+        
+
+
+
+ 
+
 
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
+my_leaf.battery.describe_battery() # calling the battery attribute and 
+# describe function 
+my_leaf.battery.get_range()
 print(my_leaf.get_descriptive_name())
+
