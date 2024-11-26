31d026f6e45fa21259245eb5c869b235880608ac
HM-bcard <study.hub@pm.me>
2024-11-27 00:21:05 +0100

    some exercises

--- Basics/Chapter_10/Chapter_10_notes_1_e1.py
+++ Basics/Chapter_10/Chapter_10_notes_1_e1.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+import os
+print(os.getcwd())
 
-path = Path('pi_digits.txt')
+path = Path("pi_digits.txt")
 contents = path.read_text()
+contents = contents.rstrip()# method chaining
 print(contents)
 
