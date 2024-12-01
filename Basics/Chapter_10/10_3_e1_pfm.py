9800c06c3dfa20c75a811cdeb8ee31a4f78712d0
HM-bcard <study.hub@pm.me>
2024-12-01 23:56:55 +0100

    main for 1.xii

--- Basics/Chapter_10/10_3_e1_pfm.py
+++ Basics/Chapter_10/10_3_e1_pfm.py
@@ -4,7 +4,7 @@ path = Path('learning_python.txt')
 file_contents = path.read_text()
 
 for line in file_contents.splitlines():
-    print(line.rstrip(),':3')
-    
+    print(line.rstrip(),':3','<3')
+
 
     
