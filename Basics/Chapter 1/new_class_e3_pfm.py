14326ef0d0726c5ce595f9c5655229550702d68b
HM-bcard <study.hub@pm.me>
2024-12-23 23:47:52 +0100

    commit to main (last)

--- Basics/Chapter 1/new_class_e3_pfm.py
+++ Basics/Chapter 1/new_class_e3_pfm.py
@@ -3,3 +3,10 @@ class Fruit(self,name,size):
         pass
     def Orange(name,size):
         print
+    def Apple(self):
+        print(f"This {self.name} has  ")
+    
+
+class Fruit2:
+    def __init__(self,name,genus,size):
+        self.name = name 
