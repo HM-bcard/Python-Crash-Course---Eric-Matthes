1e341f98cd0adade8a71247d11f245cac26edbdb
HM-bcard <study.hub@pm.me>
2025-01-09 21:59:26 +0100

    Feline Class

---
+++ Basics/Chapter_9/ClassFeline.py
@@ -0,0 +1,7 @@
+class Feline:
+    def __init__(self,size,species,fur):
+        self.size = size
+        self.species = species
+        self.fur = fur
+    def describe(self):
+        print(f"It's a {self.species}")
